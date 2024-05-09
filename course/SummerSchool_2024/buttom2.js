3995ec7563cd8bd7463c594ac5cd0b77c1f936f4
USTC-GCL-F <fire_fuxm@qq.com>
2024-05-09 18:55:28 +0800

    SummerSchool_2024

---
+++ course/SummerSchool_2024/buttom2.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+
+    document.querySelector("#hefeihuochezhan").onclick = function(){
+        document.querySelector("#hefeihuochezhan_position").scrollIntoView(true);
+    }
+    document.querySelector("#hefeigaotienanzhan").onclick = function(){
+        document.querySelector("#hefeigaotienanzhan_position").scrollIntoView(true);
+    }
+    document.querySelector("#hefeixinqiaojichang").onclick = function(){
+        document.querySelector("#hefeixinqiaojichang_position").scrollIntoView(true);
+    }
+}
