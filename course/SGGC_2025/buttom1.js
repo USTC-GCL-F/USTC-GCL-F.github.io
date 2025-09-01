5e5cba91ae48c128074b8c7c88a235133da0655f
USTC-GCL-F <fire_fuxm@qq.com>
2025-09-01 15:36:28 +0800

    sync to staff

---
+++ course/SGGC_2025/buttom1.js
@@ -0,0 +1,182 @@
+window.onload = function(){
+    /*
+        如果滚动页面也是DOM没有解决的一个问题。为了解决这个问题，浏览器实现了一下方法，
+    以方便开发人员如何更好的控制页面的滚动。在各种专有方法中，HTML5选择了scrollIntoView()
+    作为标准方法。
+        scrollIntoView()可以在所有的HTML元素上调用，通过滚动浏览器窗口或某个容器元素，
+    调用元素就可以出现在视窗中。如果给该方法传入true作为参数，或者不传入任何参数，那么
+    窗口滚动之后会让调动元素顶部和视窗顶部尽可能齐平。如果传入false作为参数，调用元素
+    会尽可能全部出现在视口中（可能的话，调用元素的底部会与视口的顶部齐平。）不过顶部
+    不一定齐平，例如：
+    //让元素可见
+    document.forms[0].scrollIntoView();
+    当页面发生变化时，一般会用这个方法来吸引用户注意力。实际上，为某个元素设置焦点也
+    会导致浏览器滚动显示获得焦点的元素。
+        支持该方法的浏览器有 IE、Firefox、Safari和Opera。
+    */
+    document.querySelector("#top1").onclick = function(){
+        document.querySelector("#top1_position").scrollIntoView(true);
+    }
+
+
+    document.querySelector("#huiyijieshao").onclick = function(){
+        document.querySelector("#huiyijieshao_position").scrollIntoView(true);
+    }
+    document.querySelector("#baogaoanpai").onclick = function(){
+        document.querySelector("#baogaoanpai_position").scrollIntoView(true);
+    }
+    document.querySelector("#baogaojieshao").onclick = function(){
+        document.querySelector("#baogaojieshao_position").scrollIntoView(true);
+    }
+
+    document.querySelector("#huiyijieshao1").onclick = function(){
+        document.querySelector("#huiyijieshao1_position").scrollIntoView(true);
+    }
+    document.querySelector("#baogaoanpai1").onclick = function(){
+        document.querySelector("#baogaoanpai1_position").scrollIntoView(true);
+    }
+    document.querySelector("#baogaojieshao1").onclick = function(){
+        document.querySelector("#baogaojieshao1_position").scrollIntoView(true);
+    }
+
+
+
+    document.querySelector("#DanielePanozzo").onclick = function(){
+        document.querySelector("#DanielePanozzo_position").scrollIntoView(true);
+    }
+    document.querySelector("#dengjiansong").onclick = function(){
+        document.querySelector("#dengjiansong_position").scrollIntoView(true);
+    }
+    document.querySelector("#sunwei").onclick = function(){
+        document.querySelector("#sunwei_position").scrollIntoView(true);
+    }
+    document.querySelector("#huanghui").onclick = function(){
+        document.querySelector("#huanghui_position").scrollIntoView(true);
+    }
+    document.querySelector("#miaoyongwei").onclick = function(){
+        document.querySelector("#miaoyongwei_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhangjuyong").onclick = function(){
+        document.querySelector("#zhangjuyong_position").scrollIntoView(true);
+    }
+    document.querySelector("#XifengGao").onclick = function(){
+        document.querySelector("#XifengGao_position").scrollIntoView(true);
+    }
+    document.querySelector("#liuyongjin").onclick = function(){
+        document.querySelector("#liuyongjin_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhouyuanfeng").onclick = function(){
+        document.querySelector("#zhouyuanfeng_position").scrollIntoView(true);
+    }
+    document.querySelector("#chenxuejin").onclick = function(){
+        document.querySelector("#chenxuejin_position").scrollIntoView(true);
+    }
+    document.querySelector("#xiaochunxia").onclick = function(){
+        document.querySelector("#xiaochunxia_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhanglei").onclick = function(){
+        document.querySelector("#zhanglei_position").scrollIntoView(true);
+    }
+
+    document.querySelector("#shuzhenyu").onclick = function(){
+        document.querySelector("#shuzhenyu_position").scrollIntoView(true);
+    }
+
+    document.querySelector("#fuxiaoming").onclick = function(){
+        document.querySelector("#fuxiaoming_position").scrollIntoView(true);
+    }
+
+
+
+    document.querySelector("#DanielePanozzo2").onclick = function(){
+        document.querySelector("#DanielePanozzo2_position").scrollIntoView(true);
+    }
+    document.querySelector("#dengjiansong2").onclick = function(){
+        document.querySelector("#dengjiansong2_position").scrollIntoView(true);
+    }
+    document.querySelector("#sunwei2").onclick = function(){
+        document.querySelector("#sunwei2_position").scrollIntoView(true);
+    }
+    document.querySelector("#huanghui2").onclick = function(){
+        document.querySelector("#huanghui2_position").scrollIntoView(true);
+    }
+    document.querySelector("#miaoyongwei2").onclick = function(){
+        document.querySelector("#miaoyongwei2_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhangjuyong2").onclick = function(){
+        document.querySelector("#zhangjuyong2_position").scrollIntoView(true);
+    }
+    document.querySelector("#XifengGao2").onclick = function(){
+        document.querySelector("#XifengGao2_position").scrollIntoView(true);
+    }
+    document.querySelector("#liuyongjin2").onclick = function(){
+        document.querySelector("#liuyongjin2_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhouyuanfeng2").onclick = function(){
+        document.querySelector("#zhouyuanfeng2_position").scrollIntoView(true);
+    }
+    document.querySelector("#chenxuejin2").onclick = function(){
+        document.querySelector("#chenxuejin2_position").scrollIntoView(true);
+    }
+    document.querySelector("#xiaochunxia2").onclick = function(){
+        document.querySelector("#xiaochunxia2_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhanglei2").onclick = function(){
+        document.querySelector("#zhanglei2_position").scrollIntoView(true);
+    }
+
+    document.querySelector("#shuzhenyu2").onclick = function(){
+        document.querySelector("#shuzhenyu2_position").scrollIntoView(true);
+    }
+
+    document.querySelector("#fuxiaoming2").onclick = function(){
+        document.querySelector("#fuxiaoming2_position").scrollIntoView(true);
+    }
+
+
+    document.querySelector("#DanielePanozzo3").onclick = function(){
+        document.querySelector("#DanielePanozzo3_position").scrollIntoView(true);
+    }
+    document.querySelector("#dengjiansong3").onclick = function(){
+        document.querySelector("#dengjiansong3_position").scrollIntoView(true);
+    }
+    document.querySelector("#sunwei3").onclick = function(){
+        document.querySelector("#sunwei3_position").scrollIntoView(true);
+    }
+    document.querySelector("#huanghui3").onclick = function(){
+        document.querySelector("#huanghui3_position").scrollIntoView(true);
+    }
+    document.querySelector("#miaoyongwei3").onclick = function(){
+        document.querySelector("#miaoyongwei3_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhangjuyong3").onclick = function(){
+        document.querySelector("#zhangjuyong3_position").scrollIntoView(true);
+    }
+    document.querySelector("#XifengGao3").onclick = function(){
+        document.querySelector("#XifengGao3_position").scrollIntoView(true);
+    }
+    document.querySelector("#liuyongjin3").onclick = function(){
+        document.querySelector("#liuyongjin_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhouyuanfeng3").onclick = function(){
+        document.querySelector("#zhouyuanfeng3_position").scrollIntoView(true);
+    }
+    document.querySelector("#chenxuejin3").onclick = function(){
+        document.querySelector("#chenxuejin3_position").scrollIntoView(true);
+    }
+    document.querySelector("#xiaochunxia3").onclick = function(){
+        document.querySelector("#xiaochunxia3_position").scrollIntoView(true);
+    }
+    document.querySelector("#zhanglei3").onclick = function(){
+        document.querySelector("#zhanglei3_position").scrollIntoView(true);
+    }
+
+    document.querySelector("#shuzhenyu3").onclick = function(){
+        document.querySelector("#shuzhenyu3_position").scrollIntoView(true);
+    }
+
+    document.querySelector("#fuxiaoming3").onclick = function(){
+        document.querySelector("#fuxiaoming3_position").scrollIntoView(true);
+    }
+
+}
---
+++ course/SummerSchool_2025/buttom.js
@@ -0,0 +1,54 @@
+window.onload = function(){
+document.querySelector("#top1").onclick = function(){document.querySelector("#top1_position").scrollIntoView(true);}
+document.querySelector("#zhaixiaoya").onclick = function(){ document.querySelector("#zhaixiaoya_position").scrollIntoView(true); }
+document.querySelector("#maweicheng").onclick = function(){ document.querySelector("#zhaixiaoya_position").scrollIntoView(true); }
+document.querySelector("#xialiang").onclick = function(){ document.querySelector("#zhaixiaoya_position").scrollIntoView(true); }
+document.querySelector("#kanghongmei").onclick = function(){ document.querySelector("#zhaixiaoya_position").scrollIntoView(true); }
+document.querySelector("#wangyingjun").onclick = function(){ document.querySelector("#zhaixiaoya_position").scrollIntoView(true); }
+document.querySelector("#hujiangbei").onclick = function(){ document.querySelector("#zhaixiaoya_position").scrollIntoView(true); }
+document.querySelector("#lulin").onclick = function(){ document.querySelector("#zhaixiaoya_position").scrollIntoView(true); }
+
+document.querySelector("#liudaoming").onclick = function(){ document.querySelector("#liudaoming_position").scrollIntoView(true); }
+
+document.querySelector("#lanlei").onclick = function(){ document.querySelector("#lanlei_position").scrollIntoView(true); }
+
+document.querySelector("#dupeng").onclick = function(){ document.querySelector("#dupeng_position").scrollIntoView(true); }
+
+document.querySelector("#chenyichen").onclick = function(){ document.querySelector("#chenyichen_position").scrollIntoView(true); }
+document.querySelector("#taku").onclick = function(){ document.querySelector("#taku_position").scrollIntoView(true); }
+
+document.querySelector("#taoye").onclick = function(){ document.querySelector("#taoye_position").scrollIntoView(true); }
+document.querySelector("#liushuai").onclick = function(){ document.querySelector("#taoye_position").scrollIntoView(true); }
+document.querySelector("#tangketan").onclick = function(){ document.querySelector("#taoye_position").scrollIntoView(true); }
+
+document.querySelector("#cuizhaopeng").onclick = function(){ document.querySelector("#cuizhaopeng_position").scrollIntoView(true); }
+document.querySelector("#pengsida").onclick = function(){ document.querySelector("#cuizhaopeng_position").scrollIntoView(true); }
+document.querySelector("#yaoyao").onclick = function(){ document.querySelector("#cuizhaopeng_position").scrollIntoView(true); }
+document.querySelector("#cuihainan").onclick = function(){ document.querySelector("#cuizhaopeng_position").scrollIntoView(true); }
+document.querySelector("#duanyueqi").onclick = function(){ document.querySelector("#cuizhaopeng_position").scrollIntoView(true); }
+document.querySelector("#xulan").onclick = function(){ document.querySelector("#cuizhaopeng_position").scrollIntoView(true); }
+
+
+document.querySelector("#xiuyuliang").onclick = function(){ document.querySelector("#xiuyuliang_position").scrollIntoView(true); }
+
+document.querySelector("#chenwenzheng").onclick = function(){ document.querySelector("#chenwenzheng_position").scrollIntoView(true); }
+
+document.querySelector("#huangjingwei").onclick = function(){ document.querySelector("#huangjingwei_position").scrollIntoView(true); }
+
+document.querySelector("#zhujunqiu").onclick = function(){ document.querySelector("#zhujunqiu_position").scrollIntoView(true); }
+
+
+document.querySelector("#liuyumeng").onclick = function(){ document.querySelector("#liuyumeng_position").scrollIntoView(true); }
+document.querySelector("#douzhiyang").onclick = function(){ document.querySelector("#liuyumeng_position").scrollIntoView(true); }
+document.querySelector("#yanglixin").onclick = function(){ document.querySelector("#liuyumeng_position").scrollIntoView(true); }
+document.querySelector("#donghao").onclick = function(){ document.querySelector("#liuyumeng_position").scrollIntoView(true); }
+document.querySelector("#huruizhen").onclick = function(){ document.querySelector("#liuyumeng_position").scrollIntoView(true); }
+document.querySelector("#xukai").onclick = function(){ document.querySelector("#liuyumeng_position").scrollIntoView(true); }
+
+
+
+/* document.querySelector("#zhangjuyong").onclick = function(){ window.open("http://staff.ustc.edu.cn/~juyong"); } */
+/* document.querySelector("#chenrenjie").onclick = function(){ window.open("http://staff.ustc.edu.cn/~renjiec"); }
+document.querySelector("#liuligang").onclick = function(){ window.open("http://staff.ustc.edu.cn/~lgliu"); } */
+
+}
